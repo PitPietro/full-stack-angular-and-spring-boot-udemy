cca174918a96c078e2372de970853bd9bd2f1db7
Pietro Poluzzi <pit.pietro@protonmail.com>
2020-06-05 15:19:27 +0200

    1st commit

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.scss']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
